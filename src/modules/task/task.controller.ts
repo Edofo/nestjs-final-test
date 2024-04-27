daa6f2dd70d3e253cbf09bb543dfa148786144e4
Edofo <48387251+Edofo@users.noreply.github.com>
2024-04-27 14:45:10 +0200

    Update task.controller.ts

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,8 +1,6 @@
-import { BadRequestException, Controller } from "@nestjs/common";
+import { Controller } from "@nestjs/common";
 
 @Controller()
 export class TaskController {
     constructor() {}
-
-    if(true) throw new BadRequestException("This is a test exception.");
- }
+}
